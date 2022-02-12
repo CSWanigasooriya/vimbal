59bf4132358c9735567081e9a1a054bd9b2f41e9
Chamath Wanigasooriya <chamathwanigasooriya@gmail.com>
2022-02-13 01:18:08 +0530

    🐳 chore(app-config): add firebase

--- apps/vimbal/src/app/root.module.ts
+++ apps/vimbal/src/app/root.module.ts
@@ -7,6 +7,24 @@ import { PROVIDERS_CONFIG } from '../config/providers.config';
 import { RootComponent } from './root.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import {
+  provideAnalytics,
+  getAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
+import { provideMessaging, getMessaging } from '@angular/fire/messaging';
+import { providePerformance, getPerformance } from '@angular/fire/performance';
+import {
+  provideRemoteConfig,
+  getRemoteConfig,
+} from '@angular/fire/remote-config';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 @NgModule({
   declarations: [RootComponent],
   imports: [
@@ -19,8 +37,18 @@ import { environment } from '../environments/environment';
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
     }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage()),
   ],
-  providers: [PROVIDERS_CONFIG],
+  providers: [PROVIDERS_CONFIG, ScreenTrackingService, UserTrackingService],
   bootstrap: [RootComponent],
 })
 export class RootModule {
--- apps/vimbal/src/environments/environment.prod.ts
+++ apps/vimbal/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'vimbal',
+    appId: '1:73265052388:web:19165cef9797f25b0d8af1',
+    storageBucket: 'vimbal.appspot.com',
+    apiKey: 'AIzaSyC995sFqZWHzQz8opRepq-Z4XndBKDdYhQ',
+    authDomain: 'vimbal.firebaseapp.com',
+    messagingSenderId: '73265052388',
+  },
   production: true,
 };
