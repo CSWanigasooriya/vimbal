7183734a70b2f2538a7bcd59a0c8046b1702f1b7
Chamath Wanigasooriya <chamathwanigasooriya@gmail.com>
2022-02-13 02:52:14 +0530

    ✨ feat(app-layout): update layout

--- apps/vimbal/src/app/layout/layout.component.ts
+++ apps/vimbal/src/app/layout/layout.component.ts
@@ -1,12 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'vimbal-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent implements OnInit {
-  constructor() {}
+export class LayoutComponent implements OnDestroy {
+  mobileQuery: MediaQueryList;
 
-  ngOnInit(): void {}
+  fillerNav = Array.from({ length: 12 }, (_, i) => `Nav Item ${i + 1}`);
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
