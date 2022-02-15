7cea49cb119d116750699daec64a61743dcb67ce
Chamath Wanigasooriya <chamath.wanigasooriya@the-ai.team>
2022-02-15 14:59:07 +0530

    🐳 chore(auth-lib): remove auth lib

--- apps/vimbal/src/app/root.module.ts
+++ apps/vimbal/src/app/root.module.ts
@@ -1,33 +1,32 @@
 import { OverlayContainer } from '@angular/cdk/overlay';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from '@vimbal/material';
-import { PROVIDERS_CONFIG } from '../config/providers.config';
-import { RootComponent } from './root.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import {
-  provideAnalytics,
   getAnalytics,
+  provideAnalytics,
   ScreenTrackingService,
   UserTrackingService,
 } from '@angular/fire/analytics';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideDatabase, getDatabase } from '@angular/fire/database';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { provideFunctions, getFunctions } from '@angular/fire/functions';
-import { provideMessaging, getMessaging } from '@angular/fire/messaging';
-import { providePerformance, getPerformance } from '@angular/fire/performance';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
 import {
-  provideRemoteConfig,
   getRemoteConfig,
+  provideRemoteConfig,
 } from '@angular/fire/remote-config';
-import { provideStorage, getStorage } from '@angular/fire/storage';
-import { RootRoutingModule } from './root.routing';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { MaterialModule } from '@vimbal/material';
+import { PROVIDERS_CONFIG } from '../config/providers.config';
+import { environment } from '../environments/environment';
 import { LayoutComponent } from './layout/layout.component';
-import { AuthModule } from '@vimbal/auth';
+import { RootComponent } from './root.component';
+import { RootRoutingModule } from './root.routing';
 @NgModule({
   declarations: [RootComponent, LayoutComponent],
   imports: [
@@ -35,7 +34,6 @@ import { AuthModule } from '@vimbal/auth';
     BrowserAnimationsModule,
     RootRoutingModule,
     MaterialModule,
-    AuthModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
--- apps/vimbal/src/app/root.routing.ts
+++ apps/vimbal/src/app/root.routing.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { authRoutes } from '@vimbal/auth';
 import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
-  { path: 'auth', children: authRoutes },
-
   {
     path: '',
     component: LayoutComponent,
