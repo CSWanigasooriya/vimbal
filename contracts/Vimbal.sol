3bbd4d27df23175414a4d28b87a9f19b822bf80d
Chamath Wanigasooriya <chamathwanigasooriya@gmail.com>
2022-03-13 22:46:08 +0530

    Auto stash before merge of "chore/truffle" and "dev"

--- contracts/Vimbal.sol
+++ contracts/Vimbal.sol
@@ -3,4 +3,82 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Vimbal {
     string public name = 'Vimbal';
+
+    uint256 public fileCount = 0;
+
+    mapping(uint256 => File) public files;
+
+    struct File {
+        uint256 id;
+        string hash;
+        string description;
+        uint256 tipAmount;
+        uint256 timestamp;
+        address payable author;
+    }
+
+    event FileCreated(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        uint256 timestamp,
+        address payable author
+    );
+
+    event FileTipped(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        uint256 timestamp,
+        address payable author
+    );
+
+    function uploadFile(string memory _fileHash, string memory _description)
+        public
+    {
+        require(bytes(_fileHash).length > 0);
+        require(bytes(_description).length > 0);
+        require(msg.sender != address(0));
+        fileCount++;
+
+        files[fileCount] = File(
+            fileCount,
+            _fileHash,
+            _description,
+            0,
+            block.timestamp,
+            payable(address(msg.sender))
+        );
+
+        emit FileCreated(
+            fileCount,
+            _fileHash,
+            _description,
+            0,
+            block.timestamp,
+            payable(address(msg.sender))
+        );
+    }
+
+    function tipFileOwner(uint256 _id) public payable {
+        require(_id > 0 && _id <= fileCount);
+
+        File memory _file = files[_id];
+        address payable _author = _file.author;
+        _author.transfer(msg.value);
+
+        _file.tipAmount = _file.tipAmount + msg.value;
+        files[_id] = _file;
+
+        emit FileTipped(
+            _id,
+            _file.hash,
+            _file.description,
+            _file.tipAmount,
+            _file.timestamp,
+            _file.author
+        );
+    }
 }
--- test/test.js
+++ test/test.js
@@ -1,6 +1,10 @@
+const assert = require('assert');
+
 const Vimbal = artifacts.require("Vimbal");
 
 require('chai').use(require('chai-as-promised')).should();
+
+
 
 contract('Vimbal', ([deployer, author, tipper]) => {
   let vimbal;
@@ -22,6 +26,30 @@ contract('Vimbal', ([deployer, author, tipper]) => {
     it('has a name', async () => {
       const name = await vimbal.name();
       assert.equal(name, 'Vimbal');
+    })
+  })
+
+
+  describe('files', async () => {
+    let result;
+    const hash = '0x47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad';
+
+    before(async () => {
+      result = await vimbal.uploadFile(hash, 'file description', { from: author });
+      fileCount = await vimbal.fileCount();
+    })
+
+    it('creates files', async () => {
+      assert.equal(fileCount, 1);
+      const event = result.logs[0].args;
+      assert.equal(event.id.toNumber(), fileCount.toNumber(), 'id is correct');
+      assert.equal(event.description, 'file description', 'description is correct');
+      assert.equal(event.author, author, 'author is correct');
+      assert.equal(event.hash, hash, 'file hash is correct');
+
+
+      await vimbal.uploadFile(hash, 'file description', { from: author }).should.be.rejected;
+
     })
   })
 })
