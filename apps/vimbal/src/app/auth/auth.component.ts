a988c5d5d95dce380693e20c5b8e3bcf6d55786f
Chamath Wanigasooriya <chamathwanigasooriya@gmail.com>
2022-03-16 04:26:52 +0530

    ✨ feat(dashboard): fetch wallet address

--- apps/vimbal/src/app/auth/auth.component.ts
+++ apps/vimbal/src/app/auth/auth.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { AuthService } from '@vimbal/service';
+import { AuthService, StorageService } from '@vimbal/service';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -13,16 +13,19 @@ export class AuthComponent {
 
   constructor(
     private store: Store<{ count: number; theme: boolean }>,
-    private _authService: AuthService
+    private _authService: AuthService,
+    private _storageService: StorageService
   ) {
     this.theme$ = this.store.select('theme');
   }
 
   connectMetaMask() {
-    this._authService.requestWalletPermission();
-    // this._authService.metaMaskStartOnBoarding();
+    // this._authService.requestWalletPermission();
     this._authService.getMetaMaskAccounts().then((accounts) => {
-      console.log(accounts);
+      if (!accounts) {
+        this._authService.metaMaskStartOnBoarding();
+      }
+      this._storageService.set('accounts', accounts);
     });
   }
 }
--- apps/vimbal/src/app/base/dashboard/dashboard.component.ts
+++ apps/vimbal/src/app/base/dashboard/dashboard.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { StorageService } from '@vimbal/service';
 @Component({
   selector: 'vimbal-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  public accounts = this._storageService.get('accounts');
+
+  constructor(private _storageService: StorageService) {}
+}
