9a17a77ed60c1bd2fe7b447734788b5675ebb3a8
NotChamath <chamathwanigasooriya@gmail.com>
2022-08-13 15:59:34 +0530

    ✨ feat(vimbal): add landing

--- apps/vimbal/src/app/base/base.module.ts
+++ apps/vimbal/src/app/base/base.module.ts
@@ -5,18 +5,18 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '@vimbal/material';
 import { PipeModule } from '@vimbal/pipe';
 import { NgxDocViewerModule } from 'ngx-doc-viewer';
+import { NgxJdenticonModule } from 'ngx-jdenticon';
 import { StarRatingComponent } from '../shared/star-rating/star-rating.component';
 import { UploaderComponent } from './../shared/uploader/uploader.component';
 import { BaseRoutingModule } from './base-routing.module';
+import { BrowseComponent } from './browse/browse.component';
 import { FeedComponent } from './components/feed/feed.component';
-import { PreviewComponent } from './preview/preview.component';
 import { SubmitComponent } from './components/submit/submit.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ReviewComponent } from './review/review.component';
-import { ProfileComponent } from './profile/profile.component';
-import { NgxJdenticonModule } from 'ngx-jdenticon';
-import { BrowseComponent } from './browse/browse.component';
 import { HomeComponent } from './home/home.component';
+import { PreviewComponent } from './preview/preview.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ReviewComponent } from './review/review.component';
 
 const baseModules = [DashboardComponent];
 
