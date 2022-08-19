15e4a4aba6ec29b97ad3933b2954dbd7ed671e45
NotChamath <chamathwanigasooriya@gmail.com>
2022-08-20 00:09:07 +0530

    🐳 chore(vimbal): update packages

--- apps/vimbal/src/app/root.module.ts
+++ apps/vimbal/src/app/root.module.ts
@@ -1,48 +1,44 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NgModule } from '@angular/core';
-import { getAnalytics, provideAnalytics } from '@angular/fire/analytics';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getDatabase, provideDatabase } from '@angular/fire/database';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getFunctions, provideFunctions } from '@angular/fire/functions';
-import { getMessaging, provideMessaging } from '@angular/fire/messaging';
-import { getPerformance, providePerformance } from '@angular/fire/performance';
-import {
-  getRemoteConfig,
-  provideRemoteConfig,
-} from '@angular/fire/remote-config';
-import { getStorage, provideStorage } from '@angular/fire/storage';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { ActionReducer, MetaReducer, StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { MaterialModule } from '@vimbal/material';
-import { localStorageSync } from 'ngrx-store-localstorage';
-import { environment } from '../environments/environment';
-import { AuthComponent } from './auth/auth.component';
-import { PROVIDERS_CONFIG } from './core/config/providers.config';
-import { counterReducer } from './core/state/counter/counter.reducer';
-import { sidebarReducer } from './core/state/sidebar/sidebar.reducer';
-import { themeReducer } from './core/state/theme/theme.reducer';
-import { LayoutComponent } from './layout/layout.component';
-import { RootComponent } from './root.component';
-import { RootRoutingModule } from './root.routing';
-import { DialogComponent } from './shared/dialog/dialog.component';
-import { ErrorComponent } from './shared/error/error.component';
-import { SheetComponent } from './shared/sheet/sheet.component';
+import { NgModule } from '@angular/core'
+import { getAnalytics, provideAnalytics } from '@angular/fire/analytics'
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app'
+import { getAuth, provideAuth } from '@angular/fire/auth'
+import { getDatabase, provideDatabase } from '@angular/fire/database'
+import { getFirestore, provideFirestore } from '@angular/fire/firestore'
+import { getFunctions, provideFunctions } from '@angular/fire/functions'
+import { getMessaging, provideMessaging } from '@angular/fire/messaging'
+import { getPerformance, providePerformance } from '@angular/fire/performance'
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config'
+import { getStorage, provideStorage } from '@angular/fire/storage'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { ServiceWorkerModule } from '@angular/service-worker'
+import { ActionReducer, MetaReducer, StoreModule } from '@ngrx/store'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+import { MaterialModule } from '@vimbal/material'
+import { ServiceModule } from '@vimbal/service'
+import { localStorageSync } from 'ngrx-store-localstorage'
+import { environment } from '../environments/environment'
+import { AuthComponent } from './auth/auth.component'
+import { PROVIDERS_CONFIG } from './core/config/providers.config'
+import { counterReducer } from './core/state/counter/counter.reducer'
+import { sidebarReducer } from './core/state/sidebar/sidebar.reducer'
+import { themeReducer } from './core/state/theme/theme.reducer'
+import { LayoutComponent } from './layout/layout.component'
+import { RootComponent } from './root.component'
+import { RootRoutingModule } from './root.routing'
+import { DialogComponent } from './shared/dialog/dialog.component'
+import { ErrorComponent } from './shared/error/error.component'
+import { SheetComponent } from './shared/sheet/sheet.component'
 
-export function localStorageSyncReducer(
-  reducer: ActionReducer<any>
-): ActionReducer<any> {
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
   return localStorageSync({
     keys: ['count', 'theme'],
     rehydrate: true,
-  })(reducer);
+  })(reducer)
 }
-const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
+const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer]
 
 @NgModule({
   declarations: [
@@ -60,6 +56,7 @@ const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
+    ServiceModule,
     StoreModule.forRoot(
       { count: counterReducer, theme: themeReducer, sidebar: sidebarReducer },
       { metaReducers: metaReducers }
--- libs/service/src/lib/service.module.ts
+++ libs/service/src/lib/service.module.ts
@@ -1,7 +1,37 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { ReviewService } from './review.service'
+import { Web3StorageService } from './web3-storage.service'
+import { AuthService } from './auth.service'
+import { ChainService } from './chain.service'
+import { FirestoreService } from './firestore.service'
+import { GlobalRippleOptionsService } from './global-ripple-options.service'
+import { IpfsService } from './ipfs.service'
+import { LoggerService } from './logger.service'
+import { StorageService } from './storage.service'
+import { WhisperService } from './whisper.service'
+
+const services = [
+  AuthService,
+  ChainService,
+  FirestoreService,
+  GlobalRippleOptionsService,
+  IpfsService,
+  LoggerService,
+  ReviewService,
+  StorageService,
+  Web3StorageService,
+  WhisperService,
+]
 
 @NgModule({
   imports: [CommonModule],
+  providers: services,
 })
-export class ServiceModule {}
+export class ServiceModule {
+  static forRoot() {
+    return {
+      ngModule: ServiceModule,
+    }
+  }
+}
